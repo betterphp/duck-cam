40485238af2b6b5df8e7047553f98ae246f592ee
Jacek Kuzemczak <jacek@kuzemczak.co.uk>
2016-04-15 22:28:49 +0100

    Start VLC in background mode

--- stream.sh
+++ stream.sh
@@ -3,4 +3,4 @@
 media_url="rtsp://192.168.1.33:554/ISAPI/streaming/channels/101?auth=YWRtaW46S2Q0ODJHaml1ZQ=="
 sout='#transcode{vcodec=theo,vb=1600,scale=1,acodec=mp3,ab=192,samplerate=44100}:http{mux=ogg,dst=127.0.0.1:8082/stream}'
 
-echo cvlc $media_url --sout $sout
+cvlc -d $media_url --sout $sout
